0c57a43b1dd421b457a8f8abcd332783fc5742de
pwndorei <lkjuio3865@naver.com>
2023-08-25 21:24:38 +0900

    debugger.ps1 complete

--- AutoMSEC.ps1
+++ AutoMSEC.ps1
@@ -9,25 +9,41 @@ if($arch -ne "x64" -and $arch -ne "x86"){
     Exit
 }
 
+if(Test-Path -Path (Join-Path $PSScriptRoot "result\"))
+{
+    (Get-ChildItem -Path $PSScriptRoot -Filter "result").Delete($true)
+}
+
+New-Item -ItemType "directory" -Path $PSScriptRoot -Name "result"
+
 $DbgShellPath = Join-Path $PSScriptRoot "DbgShell\$arch"
 $DbgShell = Join-Path $DbgShellPath "DbgShell.exe"
 $ScriptPath = Join-Path $PSScriptRoot "debugger.ps1"
 
 $inputs = Get-ChildItem $config.in 
-$cmdlines = [System.Collections.ArrayList]::new()
+$argslist = [System.Collections.ArrayList]::new()
 
 Write-Host $inputs
 Write-Host "Crashed Testcases"
-if($config.cmdline.IndexOf("@@") -ne -1){ #need args input
+if($config.args.IndexOf("@@") -ne -1){ #need args input
     for ($i = 0; $i -le $inputs.Length - 1; $i += 1){
-        $cmdlines.Add(
-            $config.cmdline.replace('@@', $inputs[$i].FullName)
+        $argslist.Add(
+            $config.args.replace('@@', $inputs[$i].FullName)
         )
     }
 }
 
-Write-Host $cmdlines
+$argslist[180] | Out-File -FilePath "./args.txt"
 
-#$debugger = (Start-Process -FilePath $Dbgshell -PassThru -ArgumentList $ScriptPath).Id
+$dpid = (Start-Process -FilePath $DbgShell -PassThru -ArgumentList $ScriptPath -NoNewWindow).Id
+Wait-Process -Id $dpid
 
-#Write-Host $debugger 
+$results = Get-ChildItem -Path (Join-Path $PSScriptRoot "result\")
+
+foreach($r in $results)
+{
+    $json = Get-Content -Path $r.FullName | ConvertFrom-Json
+
+    $ExceptionType = $json['EXCEPTION_TYPE']
+
+}
--- debugger.ps1
+++ debugger.ps1
@@ -1,6 +1,69 @@
 <#BreakCode = 2147483651 #>
+$ExitEvent = "ProcessExitedEventArgs"
+$ExceptionEvent = "ExceptionEventArgs"
+$BreakExceptionCode = 2147483651 #ignore this code
+$ExceptionCount = 0
+$ResultPath = Join-Path $PSScriptRoot "result\"
 
-param (
-    [Parameter(Mandatory=$true)]
-    [string]$cmdline
-)
+
+$config = Get-Content -Path (Join-Path $PSScriptRoot "config.json") | ConvertFrom-Json
+$arg = Get-Content -Path (Join-Path $PSScriptRoot "args.txt")
+
+
+function Save-Exception($Exception)
+{
+    if($Exception.ExceptionCode -eq $BreakExceptionCode)
+    {
+        return
+    }
+
+    $hashtable = @{}
+
+    foreach($line in !exploitable -m){
+        $idx = $line.IndexOf(":")
+
+        if($idx -eq -1)
+        {
+            continue
+        }
+        $line = $line -split ":"
+        $hashtable[$line[0]] = $line[1]
+    }
+
+    $hashtable | ConvertTo-Json | Out-File -FilePath (Join-Path $ResultPath "result$ExceptionCount.json")
+    $ExceptionCount += 1
+}
+
+$Debuggee = (Start-Process -PassThru -FilePath (Join-Path $PSScriptRoot $config.executable) -WindowStyle Hidden -ArgumentList $arg -WorkingDirectory $PSScriptRoot)
+
+Connect-Process -Id $Debuggee.Id
+
+if($Debugger.TargetIs32Bit){
+    .load (Join-Path $PSScriptRoot "MSEC_x86.dll")
+}
+
+else{
+    .load (Join-Path $PSScriptRoot "MSEC_x64.dll")
+}
+
+
+:DEBUG While($true){
+    $events = g
+    foreach($e in $events){
+        switch($e.GetType().Name)
+        {
+            $ExitEvent {break DEBUG}
+            $ExceptionEvent
+            {
+                Save-Exception -Exception $e
+                if($e.IsFirstChance)
+                {
+                    break
+                }
+                break DEBUG
+            }
+        }
+    }
+}
+
+exit
