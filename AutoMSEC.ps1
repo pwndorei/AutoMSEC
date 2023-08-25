c59ee55b373e3209f169ff5b00cef521c2d44d31
Junsu Lee <lkjuio3865@naver.com>
2023-08-25 15:40:30 +0900

    edit AutoMSEC.ps1, spawn debugger.ps1 per testcase

--- AutoMSEC.ps1
+++ AutoMSEC.ps1
@@ -3,6 +3,7 @@ param (
     [string]$arch
 )
 
+$config = Get-Content -Path (Join-Path $PSScriptRoot "config.json") | ConvertFrom-Json
 if($arch -ne "x64" -and $arch -ne "x86"){
     Write-Host "-arch must be x64 or x86"
     Exit
@@ -12,10 +13,21 @@ $DbgShellPath = Join-Path $PSScriptRoot "DbgShell\$arch"
 $DbgShell = Join-Path $DbgShellPath "DbgShell.exe"
 $ScriptPath = Join-Path $PSScriptRoot "debugger.ps1"
 
-Write-Host $DbgShellPath
-Write-Host $DbgShell
-Write-Host $ScriptPath
+$inputs = Get-ChildItem $config.in 
+$cmdlines = [System.Collections.ArrayList]::new()
 
-$debugger = (Start-Process -FilePath $Dbgshell -PassThru -ArgumentList $ScriptPath).Id
+Write-Host $inputs
+Write-Host "Crashed Testcases"
+if($config.cmdline.IndexOf("@@") -ne -1){ #need args input
+    for ($i = 0; $i -le $inputs.Length - 1; $i += 1){
+        $cmdlines.Add(
+            $config.cmdline.replace('@@', $inputs[$i].FullName)
+        )
+    }
+}
 
-Write-Host $debugger 
+Write-Host $cmdlines
+
+#$debugger = (Start-Process -FilePath $Dbgshell -PassThru -ArgumentList $ScriptPath).Id
+
+#Write-Host $debugger 
