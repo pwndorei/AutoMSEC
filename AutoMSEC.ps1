416203a928354ae9d17687409a8420ab21ae18da
pwndorei <lkjuio3865@naver.com>
2023-12-06 20:44:38 +0900

    Complete

--- AutoMSEC.ps1
+++ AutoMSEC.ps1
@@ -4,6 +4,26 @@ param (
 )
 
 $config = Get-Content -Path (Join-Path $PSScriptRoot "config.json") | ConvertFrom-Json
+
+function Move-Result($Destination, $Name)
+{
+    if( !(Test-Path -Path $Destination) )
+    {
+        New-Item -Path $Destination -ItemType Directory
+    }
+
+    $Destination = Join-Path $Destination $Name
+
+    if( !(Test-Path -Path $Destination) )
+    {
+        New-Item -Path $Destination -ItemType Directory
+    }
+
+    Get-ChildItem -File (Join-Path $PSScriptRoot "result\") | Move-Item -Destination $Destination 
+}
+
+
+
 if($arch -ne "x64" -and $arch -ne "x86"){
     Write-Host "-arch must be x64 or x86"
     Exit
@@ -21,29 +41,33 @@ $DbgShell = Join-Path $DbgShellPath "DbgShell.exe"
 $ScriptPath = Join-Path $PSScriptRoot "debugger.ps1"
 
 $inputs = Get-ChildItem $config.in 
-$argslist = [System.Collections.ArrayList]::new()
 
-Write-Host $inputs
-Write-Host "Crashed Testcases"
-if($config.args.IndexOf("@@") -ne -1){ #need args input
-    for ($i = 0; $i -le $inputs.Length - 1; $i += 1){
-        $argslist.Add(
-            $config.args.replace('@@', $inputs[$i].FullName)
-        )
-    }
+if($config.args.IndexOf("@@") -eq -1){ #need args input
+    Write-Error "@@ not found in $config.args, "
 }
 
-$argslist[180] | Out-File -FilePath "./args.txt"
-
-$dpid = (Start-Process -FilePath $DbgShell -PassThru -ArgumentList $ScriptPath -NoNewWindow).Id
-Wait-Process -Id $dpid
-
-$results = Get-ChildItem -Path (Join-Path $PSScriptRoot "result\")
-
-foreach($r in $results)
+foreach($input in $inputs)
 {
-    $json = Get-Content -Path $r.FullName | ConvertFrom-Json
+    $arg = $config.args.replace('@@', $input.FullName)
+    $arg | Out-File -FilePath "./args.txt"
+    $dpid = (Start-Process -FilePath $DbgShell -PassThru -ArgumentList $ScriptPath -NoNewWindow).Id
+    Wait-Process -Id $dpid
+    $results = Get-ChildItem -Path (Join-Path $PSScriptRoot "result\")
 
-    $ExceptionType = $json['EXCEPTION_TYPE']
+    foreach($r in $results)
+    {
+        $json = Get-Content -Path $r.FullName | ConvertFrom-Json
 
+        $Name = $json.EXCEPTION_TYPE + "_" + $json.MAJOR_HASH + "." + $json.MINOR_HASH
+        $Class = $json.CLASSIFICATION
+
+        Move-Result -Destination (Join-Path $config.out $Class) -Name $Name
+
+    }
+
+    if($results.Length -ne 0)
+    {
+        $Destination = Join-Path $config.out $Class
+        Move-Item -Path $input.Fullname -Destination (Join-Path $Destination $Name)
+    }
 }
